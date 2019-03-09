7e122f7680dcb3133db5a6133c1855a0fbd9b80e
fzea <nuttahpon_best@hotmail.com>
2019-03-09 08:41:10 +0700

    add test

--- test.py
+++ test.py
@@ -5,21 +5,50 @@ import os
 import time
 import datetime
 import glob
-
+import mysql.connector
+import Adafruit_DHT
 
 #file = open("./logfile/logfile.txt","w")
-file2 = open("./logfile/position refer.txt","w")
-file3 = open("./logfile/position change.txt","w")  
+#file2 = open("./logfile/position refer.txt","w")
+#file3 = open("./logfile/position change.txt","w")  
 print ("Car Detection System Setting ...")
 #file.write("Car Detection System Active at " + datetime.datetime.now().strftime("%y/%m/%d - %H.%M.%S\n"))
+sensor = Adafruit_DHT.AM2302
+pin = 2
+
+try:
+    connection = mysql.connector.connect(host='student.coe.phuket.psu.ac.th',
+                             database='s5835512005_403',
+                             user='s5835512005_403',
+                             password='aw38KA43')
+    cur = connection.cursor()
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Succesfully Connected to MySQL database. MySQL Server version on ", db_Info)
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+def temp():
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+    degrees = str(temperature)
+    sql = """INSERT INTO `Temp` (`Idtmp`, `Value`, `Dt`) VALUES (NULL, "{}", CURRENT_TIMESTAMP);""".format(degrees[0:4])
+    cur.execute(sql)
+    connection.commit()
 #create VideoCapture object and read from video file
+
 cap = cv2.VideoCapture(0)
+
 
 #use trained cars XML classifiers
 car_cascade = cv2.CascadeClassifier('./cascade/cascade1.xml')
 #car_cascade = cv2.CascadeClassifier('./cascade/motorbike.xml')
 
-pic_num = 0
+pic_num = 1
+count = 0
 car2 = None
 year = datetime.date.today().strftime("%Y")
 mont = datetime.date.today().strftime("%m")
@@ -51,8 +80,8 @@ def draw_flow(img, flow, step=30):
     
         for (x1, y1), (x2, y2) in lines:
             cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
-            file2.write("x1 = "+ str(x1)+"  "+"y1 = "+ str(y1)+"\t")
-            file3.write("x2 = "+ str(x2)+"  "+"y2 = "+ str(y2)+"\t")
+            #file2.write("x1 = "+ str(x1)+"  "+"y1 = "+ str(y1)+"\t")
+            #file3.write("x2 = "+ str(x2)+"  "+"y2 = "+ str(y2)+"\t")
             cv2.arrowedLine(frame, (x1,y1), (x2,y2), (0,0,255), 1) ##red
     except ValueError:
         print('You cancelled the operation.')
@@ -84,21 +113,28 @@ while True:
     #save image     
         for (x,y,w,h) in cars:
             if cars is not None:
-		#print " Detec found " + "  " + str(pic_num)
+                        print " Detec found " + "  " + str(pic_num)
                 #cv2.putText(car2, "Object: "+str(pic_num), (x - 5, y - 5), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
                         crop_img = car2[y: y + h, x: x + w] 
-                        cv2.imwrite("save_images/"+year+"/"+mont+"/"+str(pic_num)+" - " +datetime.datetime.now().strftime("%y-%m-%d-%H-%M")+".jpg",frame) 
+                        cv2.imwrite("save_images/"+year+"/"+mont+"/"+str(pic_num)+" - " +datetime.datetime.now().strftime("%y-%m-%d-%H-%M")+".jpg",frame)
+                        
+                        
     #print str(pic_num) + "  "+"Save Success"+" "+datetime.datetime.now().strftime("%y/%m/%d - %H.%M.%S") +"\n"
     #file.write(str(pic_num) + "  "+"Save Success"+" "+datetime.datetime.now().strftime("%y/%m/%d - %H.%M.%S") +"\n")
-            file2.write(str(pic_num) + "  "+"Save Success"+" "+datetime.datetime.now().strftime("%y/%m/%d - %H.%M.%S") +"\n")
-            file3.write(str(pic_num) + "  "+"Save Success"+" "+datetime.datetime.now().strftime("%y/%m/%d - %H.%M.%S") +"\n")
+            #file2.write(str(pic_num) + "  "+"Save Success"+" "+datetime.datetime.now().strftime("%y/%m/%d - %H.%M.%S") +"\n")
+            #file3.write(str(pic_num) + "  "+"Save Success"+" "+datetime.datetime.now().strftime("%y/%m/%d - %H.%M.%S") +"\n")
             pic_num += 1
 
     # Calculate the dense optical flow
         flow = cv2.calcOpticalFlowFarneback(prvs, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
         rgbImg = draw_flow(gray, flow)
-
-
+        
+        if count>50:
+            temp()
+            os.system('python ftpupload.py')
+            count = 0
+            
+        count += 1
     #print cars
     #print flow.shape
     #print "Flow : : 0"
@@ -109,10 +145,11 @@ while True:
 
     #display the resulting frame
     #cv2.imshow('video',  np.hstack((frame, rgbImg)))
-        cv2.imshow('Detect',frame)
+        #cv2.imshow('Detect',frame)
     #cv2.imshow('flow',rgbImg)
 
-        os.system('python ftpupload.py')
+        
+        
         #os.system('python tempdb.py')
     #press ESC or Q on keyboard to exit
         k = cv2.waitKey(30) & 0xff
@@ -127,12 +164,13 @@ while True:
 
 #release the videocapture object
 cap.release()
+connection.close() 
 
 #close all the frames
 cv2.destroyAllWindows()
 #file.write("End System " +datetime.datetime.now().strftime("%y/%m/%d - %H.%M.%S\n\n"))
 #file.close()
-file2.close()
-file3.close()
+#file2.close()
+#file3.close()
 print ("End System ... ")
     
