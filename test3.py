8bdd14c6cd2a20753de3ca881b8eeaf54edc9865
fzea <nuttahpon_best@hotmail.com>
2019-05-08 23:20:17 +0700

    re2

--- test3.py
+++ test3.py
@@ -7,11 +7,12 @@ import datetime
 import glob
 import mysql.connector
 import Adafruit_DHT
-#import multitasking
+import multitasking
 import signal
+import math
 
 # kill all tasks on ctrl-c
-#signal.signal(signal.SIGINT, multitasking.killall)
+signal.signal(signal.SIGINT, multitasking.killall)
 
 
 print ("Car Detection System Setting ...")
@@ -22,6 +23,7 @@ count = 0
 car2 = None
 year = datetime.date.today().strftime("%Y")
 mont = datetime.date.today().strftime("%m")
+os.system('python startsys.py')
 
 if not os.path.exists('save_images/'+year+'/'+mont):
            print ("Not Found Folder Save_images ...")
@@ -35,13 +37,12 @@ try:
                              password='aw38KA43')
     cur = connection.cursor()
     if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Succesfully Connected to MySQL database. MySQL Server version on ", db_Info)
+        print("Succesfully Connected to MySQL database")
 except Error as e:
     print("Error while connecting to MySQL", e)
 
            
-#@multitasking.task # <== this is all it takes :-)
+@multitasking.task # <== this is all it takes :-)
 def temp():
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
@@ -56,11 +57,17 @@ def temp():
     os.system('python ftpupload.py')
 	
 #@multitasking.task # <== this is all it takes :-)
-def draw_flow(img, flow, step=60):
+def draw_flow(img, flow, step=80):
     try:
         h, w = img.shape[:2]
         y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
-        fx, fy = flow[y,x].T  
+        fx, fy = flow[y,x].T
+        print ("FLOW X")
+        print fx-fy
+        
+        print ("FLOW Y")
+        print fy-fx
+
         lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
         lines = np.int32(lines + 0.5)
         vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
@@ -69,26 +76,24 @@ def draw_flow(img, flow, step=60):
         for (x1, y1), (x2, y2) in lines:
             cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
             cv2.arrowedLine(frame, (x1,y1), (x2,y2), (0,0,255), 1) ##red
-            if (y2-y1)>1:
-                #print "FLOW POS"
-                print y2-y1
-                cars = car_cascade.detectMultiScale(gray, 2, 1)
-                for (x,y,w,h) in cars:
-                    print "Dpetec found " + "  " + str(pic_num)
-                    cv2.imwrite("save_images/"+year+"/"+mont+"/"+str(pic_num)+" - " +datetime.datetime.now().strftime("%y-%m-%d-%H-%M")+".jpg",frame)
-                    pic_num += 1
+                
     except ValueError:
         print('You cancelled the operation.')
 
       
     return vis
 
-
+@multitasking.task # <== this is all it takes :-)
+def capture(img2):
+    cv2.imwrite("save_images/"+year+"/"+mont+"/"+str(pic_num)+" - " +datetime.datetime.now().strftime("%y-%m-%d-%H-%M")+".jpg",img2)
+    pic_num += 1
+    
 #create VideoCapture object and read from video file
-cap = cv2.VideoCapture()
-cap.open('rtsp://admin:admin@172.19.59.10:554/live_st1')
+cap = cv2.VideoCapture('./videos/fall2.avi')
+#cap = cv2.VideoCapture()
+#cap.open('rtsp://admin:admin@172.19.59.10:554/live_st1')
 #use trained cars XML classifiers
-car_cascade = cv2.CascadeClassifier('./cascade/cascade1.xml')
+#car_cascade = cv2.CascadeClassifier('./cascade/cascade1.xml')
 
 # Take the first frame and convert it to gray
 ret, frame1 = cap.read()
@@ -107,36 +112,18 @@ while True:
         frame = cv2.resize(frame, (600, 400))
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-        #cars = car_cascade.detectMultiScale(gray, 2, 1)
-        
-    #to draw arectangle in each cars 
-        #for (x,y,w,h) in cars:
-            #car2 = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-
-    #save image     
-        #for (x,y,w,h) in cars:
-            #if cars is not None:
-                        #print "Detec found " + "  " + str(pic_num)
-                        #crop_img = car2[y: y + h, x: x + w] 
-                        #cv2.imwrite("save_images/"+year+"/"+mont+"/"+str(pic_num)+" - " +datetime.datetime.now().strftime("%y-%m-%d-%H-%M")+".jpg",frame)
-                        
-                        
-			#print str(pic_num) + "  "+"Save Success"+" "+datetime.datetime.now().strftime("%y/%m/%d - %H.%M.%S") +"\n"
-            #pic_num += 1
-
-    # Calculate the dense optical flow
         flow = cv2.calcOpticalFlowFarneback(prvs, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
         rgbImg = draw_flow(gray, flow)
         
-        if count>30:
+        if count>10:
             temp()
             count = 0
-            pic_num = 1
             
         count += 1
     
 	#display the resulting frame
         cv2.imshow('Detect',frame)
+        #cv2.imshow('Detect',rgbImg)
         
 		
     #press ESC or Q on keyboard to exit
@@ -155,4 +142,5 @@ temp()
 cv2.destroyAllWindows()
 cap.release()
 connection.close()
+os.system('python stopsys.py')
 print ("End System ... ")
